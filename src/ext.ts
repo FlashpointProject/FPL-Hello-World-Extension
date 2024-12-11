1b1f5de555ce03efe3f93071505aa460bf615b0d
dot-mike <586280+dot-mike@users.noreply.github.com>
2024-12-11 17:45:50 +0100

    Add code

---
+++ src/ext.ts
@@ -0,0 +1,26 @@
+import * as flashpoint from 'flashpoint-launcher';
+
+export async function activate(context: flashpoint.ExtensionContext) {   
+    // Shortcut to register a command
+    function registerCmd(n: string, f: (...args: any) => any) {
+        flashpoint.registerDisposable(
+            context.subscriptions,
+            flashpoint.commands.registerCommand(n, f)
+        );
+    }
+
+    /*
+    * Register the command with name
+    */
+    registerCmd("hello-world.curation-button", async loadedCuration => {    
+        const name = flashpoint.getExtConfigValue('hello-world.name');
+
+        return await flashpoint.dialogs.showMessageBox({
+            'title': 'Hello World',
+            'message': `Hei ${name}!`,
+            'buttons': ['OK']
+        });
+
+    });
+
+};
